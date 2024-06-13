9250f2c42c92bc1c03342addea73febbcff43033
StevenDavis0218 <Isaiahdavis0218@gmail.com>
2024-06-13 15:11:19 -0400

    d
    f

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 // Declare variables below to save the different divs of your story.
-
-
-
-
-
-
-
-
-
+let buttons=document.querySelector(".buttons");
+let storystarter=document.querySelector(".story-opening");
+let moonpic=document.querySelector(".moon-pic");
+let option1=document.querySelector(".option-one");
+let option2=document.querySelector(".option-two");
+let title=document.querySelector(".title");
+let optiononescreen=document.querySelector(".option-one-screen");
+let woodsperson=document.querySelector(".woods-person");
+let optiontwoscreen=document.querySelector(".option-two-screen");
+let streetfigure=document.querySelector(".street-figure");
+let optiononeend=document.querySelector("option-one-end");
+let petergif=document.querySelector(".peter-gif");
+let optiontwoend=document.querySelector(".option-two-end");
+let jumpimage=document.querySelector(".jumped-image");
+let roadstrangeroptionone=document.querySelector(".road-stranger-option-one");
+let goodjob=document.querySelector(".good-job");
+let roadstrangeroptiontwo=document.querySelector("road-stranger-option-two");
+let game=document.querySelector(".gaming");
+let option11=document.querySelector(".option-one1");
+let option21=document.querySelector(".option-two2");
+let option12=document.querySelector(".option-one2");
+let option22=document.querySelector(".option-two2");
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
 
 
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
+option1.addEventListener('click', function(){
+    optiononescreen.style.display="block";
+    storystarter.style.display="none";
+});
 
 // INSERT_VARIABLE.addEventListener('click', function(){
 
